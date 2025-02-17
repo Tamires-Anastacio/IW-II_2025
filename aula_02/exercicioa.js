b57c2ef64ef0d83c8e2640407962289a7bbba330
Tamires <159663594+Tamires-Anastacio@users.noreply.github.com>
2025-02-17 12:41:55 -0300

    Add files via upload

---
+++ aula_02/exercicioa.js
@@ -0,0 +1,35 @@
+console.log ("Exercicio a")
+var a = 10 
+var b = 20
+console.log(b);
+var b = 5;
+console.log(a,b);
+
+//Exercicio b
+console.log ("Exercicio b")
+var a = 30
+var b = 20
+var c = a + b
+console.log(c);
+var b = 10;
+console.log(b,c);
+var c =  a + b ;
+console.log(a,b,c);
+
+//Exercicio c 
+console.log ("Exercicio c")
+var a = 10;
+var b = 20;
+var c = a;
+var b = c;
+var a = b;
+console.log (a,b,c);
+
+//Exercicio d
+console.log ("Exercicio d")
+var a = 10;
+var b = a + 1;
+var a = b + 1;
+var b = a + 1;
+console.log(a,b);
+
